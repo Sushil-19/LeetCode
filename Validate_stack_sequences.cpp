e6b55c66842072ad599e1f3c14a75544247b5846
Sushil-19 <96721697+Sushil-19@users.noreply.github.com>
2022-03-16 18:17:40 +0530

    Create Validate_stack_sequences.cpp

---
+++ Validate_stack_sequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack <int> s;
+        int j=0;
+        
+        for (auto val : pushed){
+            s.push(val);
+            while (s.size()>0 && s.top() == popped[j]){
+                s.pop();
+                j++;
+            }
+        }
+        return s.size()== 0;
+    }
+};
